032190031c179716a667c0fc3b7290a0c751cdbd
Anderson Araujo <anderson.araujo@gmail.com>
2016-02-22 17:31:02 -0300

    Added validation handling.

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -11,16 +11,32 @@ module.exports = function (app) {
                     response.json(result);
                 }
             });
-
         });
     });
 
     app.get('/produtos/form', function (request, response) {
-        response.render('produtos/form');
+        response.render('produtos/form', {produto: {}, validationErrors: {}});
     });
 
     app.post('/produtos', function (request, response) {
         var produto = request.body;
+
+        request.assert('titulo', 'Titulo é obrigatório').notEmpty();
+        request.assert('preco', 'Formato inválido').isFloat();
+
+        var errors = request.validationErrors();
+        if (errors) {
+            response.format({
+                html: function () {
+                    response.status(400).render('produtos/form', {produto: produto, validationErrors: errors});
+                },
+                json: function () {
+                    response.status(400).json(errors);
+                }
+            });
+            return;
+        }
+
         var produtosDAO = new app.infra.ProdutosDAO(app.infra.connectionFactory());
         produtosDAO.salva(produto, function (err, result) {
             response.redirect('/produtos');
