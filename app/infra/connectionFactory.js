3a8f6cceaf2bcc32c6cc51969bbaf52f557af3c2
Anderson Araujo <anderson.araujo@gmail.com>
2016-02-22 19:33:15 -0300

    Added static content.

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,12 +1,31 @@
 var mysql = require('mysql');
 
 function createDbConnection() {
-    return mysql.createConnection({
-        host: 'localhost',
-        user: 'casadocodigo',
-        password: 'casadocodigo',
-        database: 'casadocodigo_nodejs'
-    });
+    if (process.env.NODE_ENV == 'test') {
+        return mysql.createConnection({
+            host: 'localhost',
+            user: 'casadocodigo',
+            password: 'casadocodigo',
+            database: 'casadocodigo_nodejs_test'
+        });
+
+    } else if (process.env.NODE_ENV == 'production') {
+        return mysql.createConnection({
+            host: 'localhost',
+            user: 'casadocodigo',
+            password: 'casadocodigo',
+            database: 'casadocodigo_nodejs_prd'
+        });
+
+    } else {
+        return mysql.createConnection({
+            host: 'localhost',
+            user: 'casadocodigo',
+            password: 'casadocodigo',
+            database: 'casadocodigo_nodejs'
+        });
+    }
+
 }
 
 module.exports = function () {
