2f0dfb73330a5dfd98c7f7e182d3715b4b9df4bf
Anderson Araujo <anderson.araujo@gmail.com>
2016-02-23 13:38:10 -0300

    Added websocket with socket.io.

---
+++ app/routes/promocoesController.js
@@ -0,0 +1,26 @@
+module.exports = function (app) {
+
+    app.get('/promocoes/form', function (request, response) {
+        var produtosDAO = new app.infra.ProdutosDAO(app.infra.connectionFactory());
+        produtosDAO.lista(function (err, result) {
+            if (err) {
+                return next(err);
+            }
+            response.format({
+                html: function () {
+                    response.render('promocoes/form', {'lista': result, validationErrors: {}});
+                },
+                json: function () {
+                    response.json(result);
+                }
+            });
+        });
+    });
+
+    app.post('/promocoes', function (request, response, next) {
+        var promocao = request.body;
+        app.get('io').emit('novaPromocao', promocao);
+        response.redirect('/promocoes/form');
+    });
+
+};
