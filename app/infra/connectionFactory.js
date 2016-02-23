4068b95ae47c2be569203716a649bb976e394c62
Anderson Araujo <anderson.araujo@gmail.com>
2016-02-23 19:42:00 -0300

    DB url from env variable.

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -10,11 +10,13 @@ function createDbConnection() {
         });
 
     } else if (process.env.NODE_ENV == 'production') {
+        var urlConexao = process.env.CLEARDB_DATABASE_URL;
+        var grupos = urlConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
         return mysql.createConnection({
-            host: 'localhost',
-            user: 'casadocodigo',
-            password: 'casadocodigo',
-            database: 'casadocodigo_nodejs_prd'
+            host: grupos[3],
+            user: grupos[1],
+            password: grupos[2],
+            database: grupos[4]
         });
 
     } else {
